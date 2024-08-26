41eb4fe08ed988962f6f8eb651d4203c65f8f365
IshraqueSarwar <sarwarishraque@yahoo.com>
2024-08-26 23:40:19 +0600

    main

--- cls.cpp
+++ cls.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 // main constructor for 1-dimensional tensor
 Tensor1D::Tensor1D():tensor(){}
+Tensor1D::Tensor1D(const Tensor1D& other): tensor(other.tensor){}
+		
+		
+
 
 Tensor1D::Tensor1D(vector<float> t1d){
 	Tensor1D::tensor = t1d;
@@ -17,20 +21,31 @@ Tensor1D::Tensor1D(vector<float> t1d){
 //main constructors for 2-dimensional tensor
 Tensor2D::Tensor2D(): tensor() {}
 
-Tensor2D::Tensor2D(vector<vector<float>> t2d){
-	Tensor2D::tensor = t2d;
+Tensor2D::Tensor2D(vector<vector<float>> t2d, bool validify){
+	if(Tensor2D::valid_tensor(t2d)){
+		Tensor2D::tensor = t2d;
+	}else{
+		if(validify){
+			Tensor2D::tensor = Tensor2D::fill_to_valid(t2d);
+			return;
+		}
+		throw invalid_argument("error: Tensor shape is not right.");
+	}
 }
 
 
 int main(int argc, char* argv[]){
-	Tensor1D tensor({1.5,4.6, 5.6});
-	// tensor.print();
+	Tensor1D t({1.5,4.6, 5.6});
+	Tensor1D tr;
+	tr =t+1.5;
+	tr.print();
 
 
-	Tensor2D tensor2({{1.3,1.3,2.5}, {6.3,4.3,3.4}});
-	// tensor2.dim();
-	// tensor2.transpose();
-	// tensor2.dim();
+
+	Tensor2D t2({{1.3,1.3,2.5}, {2.3,4.3,3.4}});
+	t2.get();
+	// t2.transpose();
+	// t2.dim();
 
 
 
--- cls.h
+++ cls.h
@@ -1,11 +1,26 @@
 #ifndef CLS_H
 #define CLS_H
-
+#include <stdexcept>
 
 using namespace std;
 
 
 //TODO BOARD:
+	// 1. operator overloading:(for 1D Tensor) 
+		// |--->equal(for assiging another tensor) X
+		// |--->addition-auto type X
+		// |--->addition(w/another tensor)
+		// |--->
+		// |--->
+		// |--->
+		// |--->
+	// 2. MAKE THE OVERLOAD WORK VICEVERSA.
+	
+
+
+
+
+// DONE:
 	// 1. Add validation while a new tensor is created making sure the dimensions are correct
 		// |
 		// |
@@ -19,7 +34,7 @@ using namespace std;
 				// |--->a) 1D vs int
 				// 		b) 1D vs matrix --> check dimensions
 				//		c) matrix vs matrix -->check dimenstions
-
+	
 
 class NNL{
 
@@ -47,6 +62,7 @@ class NNL{
 
 
 // 2d tensor class
+// DOC: 
 class Tensor2D: public NNL{
 	vector<vector<float>> tensor;
 	vector<int>dimensions;
@@ -54,13 +70,54 @@ class Tensor2D: public NNL{
 
 	public:
 		Tensor2D();
-		Tensor2D(vector<vector<float>>t2d);
+		Tensor2D(vector<vector<float>>t2d, bool validify = false);
 
-		//TODO: getter func
+
+		vector<vector<float>> fill_to_valid(vector<vector<float>> t){
+			int mx = 0;
+			for(int i=0;i<t.size();i++){
+				int sz = t[i].size();
+				mx = max(mx, sz);
+			}
+
+			for(int i=0;i<t.size();i++){
+				if(t[i].size()<mx){
+					int dif = mx-t[i].size();
+					while(dif){
+						t[i].push_back(0.0);
+						dif--;
+					}
+				}
+			}
+
+			return t;
+		}
+
+		bool valid_tensor(vector<vector<float>> t){
+			if(!t.empty()){
+				int col = t[0].size();
+
+				for(int i=0;i<t.size();i++){
+					if(t[i].size()!=col){
+						return false;
+					}
+				}
+				return true;
+			}
+
+			return false;
+		}
+
+		
 
 		//setter func
 		void set(vector<vector<float>> t2d){
 			tensor = t2d;
+		}
+
+		//getter func
+		Tensor2D get(){
+			return tensor;
 		}
 
 
@@ -120,10 +177,19 @@ class Tensor2D: public NNL{
 		}
 
 
+
+	//operation overload
+	// Tensor2D operator+(int& n){
+
+	// }
+
 	private:
 
 
 };
+
+
+
 
 
 // 1d tensor class
@@ -137,6 +203,8 @@ class Tensor1D: public NNL{
 		// main constructor
 		Tensor1D();
 		Tensor1D(vector<float> t1d);
+
+		Tensor1D(const Tensor1D& other);
 		
 		
 
@@ -145,10 +213,14 @@ class Tensor1D: public NNL{
 
 
 		//setter
-		void set(vector<float> t1d){
-			tensor = t1d;
+		void set(const Tensor1D& other){
+			tensor = other.tensor;
 		}
 
+		//getter
+		Tensor1D get(){
+			return tensor;
+		}
 
 
 
@@ -175,6 +247,37 @@ class Tensor1D: public NNL{
 			NNL::printTensorDim(dimensions);
 			return dimensions;
 		}
+
+		//overloading
+		Tensor1D operator+(auto n)const{
+			Tensor1D res;
+
+			for(int i = 0;i<tensor.size();i++){
+				res.tensor.push_back(tensor[i]+n);
+			}
+			
+			return res;
+		}
+		// Tensor1D operator+(double n){
+		// 	Tensor1D res;
+
+		// 	for(int i = 0;i<tensor.size();i++){
+		// 		res.tensor.push_back(tensor[i]+n);
+		// 	}
+			
+		// 	return res;
+		// }
+		friend Tensor1D operator+(auto n, Tensor1D& t){
+			return t+n;
+		}
+
+
+
+		Tensor1D operator=(const Tensor1D& other){
+			tensor = other.tensor;
+			return tensor;
+		}
+
 
 };
 
